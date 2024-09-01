d3fba68f318394a59f4e39b7e617f27cc36dd6a5
lokeshthatipamula <thatipamulalokesh313@gmail.com>
2024-09-01 17:30:53 +0530

    Adding codes

---
+++ ttt.js
@@ -0,0 +1,66 @@
+let boxes=document.querySelectorAll(".box");
+let reset=document.querySelector("#reset");
+let newGame=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-contain");
+let msg=document.querySelector("#msg");
+
+let turnX=true;
+
+const winPatterns=[
+    [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8],
+];
+
+const resetGame=()=>{
+    turnX=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("clicked!");
+        if(turnX){
+            box.innerText="X";
+            turnX=false;
+        }else{
+            box.innerText="O";
+            turnX=true;
+        }  
+        box.disabled=true;
+
+        checkWinner();
+    });
+});
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulations! Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+const checkWinner=()=>{
+    for(let pattern of winPatterns){
+        let pos1Val=boxes[pattern[0]].innerText;
+        let pos2Val=boxes[pattern[1]].innerText;
+        let pos3Val=boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("Winner :",pos1Val);
+                showWinner(pos1Val);
+            }
+        }  
+    }
+};
+
+newGame.addEventListener("click",(resetGame));
+reset.addEventListener("click",(resetGame));
